e70dd05b0c741c9b2c5dd13952289f4a96191e8b
Ved Gupta <vedgupta@protonmail.com>
2022-03-23 17:07:40 +0530

    logo update anfd png img added

--- pages/index.js
+++ pages/index.js
@@ -88,8 +88,8 @@ export default function Home() {
           className="h-auto mx-auto mt-24 cursor-pointer"
           src="/logo__1_-removebg-preview.png"
           alt='Contkt Logo'
-          width={200}
-          height={200}
+          width={220}
+          height={220}
         />
         <SendMessage />
         <Messages />
